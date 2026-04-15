c78c2f59925d97c2d61cdeab0431fa15081449c4
SAIEED12 <abusaieed0@gmail.com>
2026-04-15 19:32:42 +0600

    Added Timeline Component

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -1,23 +1,59 @@
-'use client'
-import { TimelineContext } from '@/context/timeline.context';
-import React, { useContext } from 'react';
+"use client";
+import { TimelineContext } from "@/context/timeline.context";
+import React, { useContext } from "react";
+import call from "@/assets/call.png";
+import text from "@/assets/text.png";
+import video from "@/assets/video.png";
+import Image from "next/image";
 
 // export const metadata = {
 //   title: "Timeline | KinKeeper",
 //   description: "your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.",
 // };
 
-
-
 const TimelinePage = () => {
+  const { timeline, setTimeline } = useContext(TimelineContext);
 
-    const{timeline, setTimeline} = useContext(TimelineContext)
+  const getIcon = (type) => {
+  if (type === "Call") return <Image src={call} alt="Call" className="w-6 h-6" />;
+  if (type === "Text") return <Image src={text} alt="Text" className="w-6 h-6" />;
+  if (type === "Video") return <Image src={video} alt="Video" className="w-6 h-6" />;
+};
 
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="container mx-auto p-10">
+      <h2 className="text-5xl font-bold mb-8 mt-20">Timeline</h2>
+      <div className=" w-full sm:w-64 mb-10">
+        <select className="w-full border border-gray-200 rounded-xl px-4 py-3 text-black bg-white cursor-pointer focus:outline-none font-semibold">
+          <option disabled selected>
+            Filter timeline
+          </option>
+          <option value="call">Call</option>
+          <option value="text">Text</option>
+          <option value="video">Video</option>
+        </select>
+     
+      </div>
+      {timeline.map((item) => (
+  <div key={item.id} className="flex gap-4 mb-4 container mx-auto p-4 bg-white rounded-lg shadow-sm ">
+    <div className="text-2xl flex justify-center items-center">{getIcon(item.type)}</div>
+
+    <div>
+      <h3 className="font-semibold">
+        {item.type} with {item.name}
+      </h3>
+      <p className="text-sm text-gray-500">
+        {new Date(item.date).toLocaleString("en-US", {
+          month: "short",
+          day: "numeric",
+        year: "numeric",
+        })}
+      </p>
+    </div>
+  </div>
+))}
+    </div>
+  );
 };
 
 export default TimelinePage;
--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -6,12 +6,18 @@ import { PiVideoCamera } from "react-icons/pi";
 import { TimelineContext } from "@/context/timeline.context";
 import { toast, Zoom } from "react-toastify";
 
-const ToggleButton = ({friend}) => {
+const ToggleButton = ({ friend }) => {
   const { timeline, setTimeline } = useContext(TimelineContext);
 
-  const handleCall = () => {
-    setTimeline([...timeline, friend]);
-    toast.success(`Call with ${ friend.name}`, {
+  const handleAction = (friend, action) => {
+    const log = {
+      id: Date.now(),
+      name: friend.name,
+      type: action,
+      date: new Date(),
+    };
+    setTimeline([...timeline, log]);
+    toast.success(`${action} with ${friend.name}`, {
       position: "top-center",
       autoClose: 2000,
       hideProgressBar: false,
@@ -23,53 +29,27 @@ const ToggleButton = ({friend}) => {
       transition: Zoom,
     });
   };
-  const handleText = () => {
-    setTimeline([...timeline, friend]);
-    toast.success(`Text with ${ friend.name}`, {
-      position: "top-center",
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: false,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "dark",
-      transition: Zoom,
-    });
-  };
-  const handleVideoCall = () => {
-    setTimeline([...timeline, friend]);
-    toast.success(`Video Call with ${ friend.name}`, {
-      position: "top-center",
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: false,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "dark",
-      transition: Zoom,
-    });
-  };
+
+ 
 
   return (
     <div className="grid grid-cols-3 gap-4">
       <button
-        onClick={handleCall}
+        onClick={() => handleAction(friend, "Call")}
         className="border border-gray-200 rounded-xl p-4 flex flex-col items-center gap-2 cursor-pointer hover:bg-gray-50"
       >
         <BiPhoneCall className="text-2xl text-[#1F2937]" />
         <span className="text-sm text-[#1F2937]">Call</span>
       </button>
       <button
-        onClick={handleText}
+        onClick={() => handleAction(friend, "Text")}
         className="border border-gray-200 rounded-xl p-4 flex flex-col items-center gap-2 cursor-pointer hover:bg-gray-50"
       >
         <RiMessage2Line className="text-2xl text-[#1F2937]" />
         <span className="text-sm text-[#1F2937]">Text</span>
       </button>
       <button
-        onClick={handleVideoCall}
+        onClick={() => handleAction(friend, "Video")}
         className="border border-gray-200 rounded-xl p-4 flex flex-col items-center gap-2 cursor-pointer hover:bg-gray-50"
       >
         <PiVideoCamera className="text-2xl text-[#1F2937]" />
